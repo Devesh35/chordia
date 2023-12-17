13ab86cdb8fd716550b4f6ea76407deb4c70fb11
Devesh35 <devesh46singh@gmail.com>
2023-12-17 16:44:58 +0530

    feat: Added table

--- libs/design/elements/src/lib/lists/Table.tsx
+++ libs/design/elements/src/lib/lists/Table.tsx
@@ -1,3 +1,39 @@
-export const Table = () => {
-  return <div>Table</div>;
-};
+import React from 'react';
+
+type TableColumn = { id: string; name: string };
+type TableData = Record<string, React.ReactNode>[];
+
+interface TableProps {
+  data: TableData;
+  columns: TableColumn[];
+  onRowClick?: (row: TableData[number]) => void;
+  onCellClick?: (row: TableData[number], columnKey: string) => void;
+}
+
+export const Table: React.FC<TableProps> = ({
+  data,
+  columns,
+  onRowClick,
+  onCellClick,
+}) => (
+  <table>
+    <thead>
+      <tr>
+        {columns.map(({ id, name }) => (
+          <th key={id}>{name}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {data.map((row, rowIndex) => (
+        <tr key={rowIndex} onClick={() => onRowClick?.(row)}>
+          {columns.map(({ id: key }) => (
+            <td key={key} onClick={() => onCellClick?.(row, key)}>
+              {row[key]}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
--- libs/design/elements/src/lib/lists/index.ts
+++ libs/design/elements/src/lib/lists/index.ts
@@ -1,3 +1,4 @@
 export { Accordion } from './Accordion';
 export { NestedAccordion } from './NestedAccordion';
+export { Table } from './Table';
 export { TagGroup } from './TagGroup';
